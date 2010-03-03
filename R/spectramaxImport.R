5074694707d3134087754f308c2bb20351a758b1
David Zhang <j.zhang@dkfz.de>
2010-03-03 15:20:34 +0000

    Add spectramaxImport function to import Spectramax plate reader files, for RTCA validating WST-1 assay
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RTCA@45006 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/spectramaxImport.R
@@ -0,0 +1,32 @@
+parseSpectramaxText <- function(txt) {
+  body <- grep("^\tTemperature",txt) + c(1,-2)
+  sp  = strsplit(txt[seq(from=body[1], to=body[2])], "\t")
+  rawMatrix <- do.call("rbind", sp)
+  valueMatrix <- rawMatrix[,-c(1,2,ncol(rawMatrix))]
+  ## comma
+  if (length(grep(",", valueMatrix)) > 1) {
+    valueMatrix <- gsub(",",".", valueMatrix)
+  }
+  nrow <- nrow(valueMatrix)
+  ncol <- ncol(valueMatrix)
+  
+  cns <- sprintf("%02d", 1:ncol)
+  rns <- LETTERS[1:nrow]
+  
+  ## by column
+  well <- paste(rns, rep(cns,each=length(rns)),sep="")
+  val <- as.numeric(valueMatrix)
+  out <- list(data.frame(well = I(well),
+                         val = as.numeric(val)),
+              txt = I(txt))
+  return(out)
+}
+
+spectramaxImport <- function(file, encoding="latin1") {
+  txt <- lapply(file, readLines, encoding=encoding)
+  outs <- lapply(txt, parseSpectramaxText)
+  if(length(file)==1) {
+    outs <- outs[[1]]
+  }
+  return(outs)
+}
